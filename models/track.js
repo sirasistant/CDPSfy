fc34fd92d6beb117e750f9a903e78b73666d8e52
sirasistant <sirasistant@gmail.com>
2015-12-27 15:47:56 +0100

    removed default tracks

--- models/track.js
+++ models/track.js
@@ -10,20 +10,4 @@ track_id: {
 */
 
 exports.tracks = {
-	1: {
-		name: 'Cute',
-		url: '/media/Cute.mp3'
-	},
-	2: {
-		name: 'Dubstep',
-		url: '/media/Dubstep.mp3'
-	},
-	3: {
-		name: 'Epic',
-		url: '/media/Epic.mp3'
-	},
-	4: {
-		name: 'Littleidea',
-		url: '/media/Littleidea.mp3'
-	}
 };
